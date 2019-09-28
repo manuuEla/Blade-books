638706936d6762e075e258ed31ffd373e775b49f
manuuEla <53089429+manuuEla@users.noreply.github.com>
2019-09-28 16:05:04 +0300

    Add files via upload

--- book.blade.php
+++ book.blade.php
@@ -1,32 +1,32 @@
 <!-- Modal -->
-<div class="modal fade" id="edit-book-{{$book->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="add-book" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h3 class="modal-title" id="exampleModalLabel">Book</h3>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <form action="/dashboard/book/save/{{$book->id}}" method="post">
+                <form action="/dashboard/book/add" method="post" enctype="multipart/form-data">
                     {{csrf_field()}}
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <input type="text" placeholder="TITLE..." name="title" class="form-control" value="{{$book->title}}">
+                                <input type="text" placeholder="TITLE..." name="title" class="form-control">
                             </div>
                         </div>
                     </div>
 
+
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <select name="category_id" class="form-control">
+                                <select name="author_id" class="form-control">
                                     <option value="">--Va rugam alegeti o optiune--</option>
                                     @foreach(\App\Author::all() as $author)
-                                        <option @if($author->id==$book->author_id) selected @endif
-                                        value="{{$author->id}}">{{$author->name}}</option>
+                                        <option value="{{$author->id}}">{{$author->name}}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -39,8 +39,7 @@
                                 <select name="category_id" class="form-control">
                                     <option value="">--Va rugam alegeti o optiune--</option>
                                     @foreach(\App\Category::all() as $category)
-                                        <option @if($category->id==$book->category_id) selected @endif
-                                        value="{{$category->id}}">{{$category->name}}</option>
+                                        <option value="{{$category->id}}">{{$category->name}}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -50,7 +49,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <textarea placeholder="DESCRIPTION..." name="description" class="form-control" ></textarea>
+                                <textarea placeholder="DESCRIPTION..." name="description" class="form-control"></textarea>
                             </div>
                         </div>
                     </div>
@@ -58,7 +57,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <input type="text" placeholder="ISBN..." name="isbn" class="form-control" value="{{$book->isbn}}">
+                                <input type="text" placeholder="ISBN..." name="isbn" class="form-control">
                             </div>
                         </div>
                     </div>
@@ -66,7 +65,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <input type="text" placeholder="PUBLISHER..." name="publisher" class="form-control" value="{{$book->publisher}}">
+                                <input type="text" placeholder="PUBLISHER..." name="publisher" class="form-control">
                             </div>
                         </div>
                     </div>
@@ -74,11 +73,10 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <input type="date" placeholder="RELEASED..." name="released" class="form-control" value="{{$book->released}}">
+                                <input type="date" placeholder="RELEASED..." name="released" class="form-control">
                             </div>
                         </div>
                     </div>
-
 
                     <div class="row">
                         <div class="col-md-12">
@@ -87,6 +85,7 @@
                             </div>
                         </div>
                     </div>
+
 
 
                     <div class="row">
@@ -100,10 +99,12 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <button type="submit" class="btn btn-info form-control">SAVE</button>
+                                <button type="submit" class="btn btn-info form-control">ADD</button>
                             </div>
                         </div>
+
                     </div>
+
                 </form>
             </div>
 
