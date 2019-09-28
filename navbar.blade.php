4272009f38fd3af47ec4f088152e6a82150dd950
manuuEla <53089429+manuuEla@users.noreply.github.com>
2019-09-28 16:01:10 +0300

    Add files via upload

---
+++ navbar.blade.php
@@ -0,0 +1,142 @@
+<!-- Messages: style can be found in dropdown.less-->
+<li class="dropdown messages-menu">
+    <!-- Menu toggle button -->
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <i class="fa fa-envelope-o"></i>
+        <span class="label label-success"></span>
+    </a>
+    <ul class="dropdown-menu">
+        <li class="header"></li>
+        <li>
+            <!-- inner menu: contains the messages -->
+            <ul class="menu">
+                <li><!-- start message -->
+                    <a href="#">
+                        <div class="pull-left">
+                            <!-- User Image -->
+                        </div>
+                        <!-- Message title and timestamp -->
+                        <h4>
+                            Support Team
+                            <small><i class="fa fa-clock-o"></i> </small>
+                        </h4>
+                        <!-- The message -->
+                        <p>Why not buy a new awesome theme?</p>
+                    </a>
+                </li>
+                <!-- end message -->
+            </ul>
+            <!-- /.menu -->
+        </li>
+        <li class="footer"><a href="#">See All Messages</a></li>
+    </ul>
+</li>
+<!-- /.messages-menu -->
+
+<!-- Notifications Menu -->
+<li class="dropdown notifications-menu">
+    <!-- Menu toggle button -->
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <i class="fa fa-bell-o"></i>
+        <span class="label label-warning"></span>
+    </a>
+    <ul class="dropdown-menu">
+        <li class="header"></li>
+        <li>
+            <!-- Inner Menu: contains the notifications -->
+            <ul class="menu">
+                <li><!-- start notification -->
+                    <a href="#">
+                        <i class="fa fa-users text-aqua"></i>
+                    </a>
+                </li>
+                <!-- end notification -->
+            </ul>
+        </li>
+        <li class="footer"><a href="#">View all</a></li>
+    </ul>
+</li>
+<!-- Tasks Menu -->
+<li class="dropdown tasks-menu">
+    <!-- Menu Toggle Button -->
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <i class="fa fa-flag-o"></i>
+        <span class="label label-danger"></span>
+    </a>
+    <ul class="dropdown-menu">
+        <li class="header"></li>
+        <li>
+            <!-- Inner menu: contains the tasks -->
+            <ul class="menu">
+                <li><!-- Task item -->
+                    <a href="#">
+                        <!-- Task title and progress text -->
+                        <h3>
+                            Design some buttons
+                            <small class="pull-right"></small>
+                        </h3>
+                        <!-- The progress bar -->
+                        <div class="progress xs">
+                            <!-- Change the css width attribute to simulate progress -->
+                            <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
+                                 aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                                <span class="sr-only">20% Complete</span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <!-- end task item -->
+            </ul>
+        </li>
+        <li class="footer">
+            <a href="#">View all tasks</a>
+        </li>
+    </ul>
+</li>
+<!-- User Account Menu -->
+<li class="dropdown user user-menu">
+    <!-- Menu Toggle Button -->
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <!-- The user image in the navbar-->
+        <!-- hidden-xs hides the username on small devices so only the image appears. -->
+        <span class="hidden-xs">Manu</span>
+    </a>
+    <ul class="dropdown-menu">
+        <!-- The user image in the menu -->
+        <li class="user-header">
+
+            <p>
+                Manu - Web Developer
+                <small>Member since 2019</small>
+            </p>
+        </li>
+        <!-- Menu Body -->
+        <li class="user-body">
+            <div class="row">
+                <div class="col-xs-4 text-center">
+                    <a href="#">Followers</a>
+                </div>
+                <div class="col-xs-4 text-center">
+                    <a href="#">Sales</a>
+                </div>
+                <div class="col-xs-4 text-center">
+                    <a href="#">Friends</a>
+                </div>
+            </div>
+            <!-- /.row -->
+        </li>
+        <!-- Menu Footer-->
+        <li class="user-footer">
+            <div class="pull-left">
+                <a href="#" class="btn btn-default btn-flat">Profile</a>
+            </div>
+            <div class="pull-right">
+                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+            </div>
+        </li>
+    </ul>
+</li>
+<!-- Control Sidebar Toggle Button -->
+<li>
+    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+</li>
